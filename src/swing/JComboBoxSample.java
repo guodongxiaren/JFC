e6ad556fe18cd144b2946e86bd2926eb1ebc95dc
guodongxiaren <879231132@qq.com>
2014-12-02 20:12:49 +0800

    更新

--- src/swing/JComboBoxSample.java
+++ src/swing/JComboBoxSample.java
@@ -3,28 +3,28 @@ package swing;
 import java.awt.BorderLayout;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Vector;
 
 import javax.swing.JComboBox;
 
 @SuppressWarnings("serial")
 public class JComboBoxSample extends Sample {
 	private JComboBox<String> cb;
+
 	private String[] you = { "苍井空", "吉泽明步", "松岛枫" };
 
 	public JComboBoxSample() {
 		cb = new JComboBox<String>(you);
-		cb.addItemListener(new ItemListener() {
-
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				// 不加if判断，则会执行两次监听方法
-				if (e.getStateChange() == ItemEvent.SELECTED)
-					println(cb.getSelectedItem());
-
-			}
-		});
+		cb.addItemListener(new ComboItemListener(cb));
 		println(cb.getSelectedItem());
 		add(cb, BorderLayout.NORTH);
+		Vector<String> vs = new Vector<>();
+		vs.add(you[0]);
+		vs.add(you[1]);
+		vs.add(you[2]);
+		JComboBox<String> cbv = new JComboBox<>(vs);
+		cbv.addItemListener(new ComboItemListener(cbv));
+		add(cbv);
 	}
 
 	public static void main(String[] args) {
@@ -32,4 +32,20 @@ public class JComboBoxSample extends Sample {
 		spl.setSize(200, 100);
 	}
 
+	class ComboItemListener implements ItemListener {
+		private JComboBox<?> cb;
+
+		public ComboItemListener(JComboBox<?> cb) {
+			this.cb = cb;
+		}
+
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			// 不加if判断，则会执行两次监听方法
+			if (e.getStateChange() == ItemEvent.SELECTED)
+				println(cb.getSelectedItem());
+
+		}
+
+	}
 }
---
+++ src/swing/JTextFieldSample.java
@@ -0,0 +1,33 @@
+package swing;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class JTextFieldSample extends Sample {
+	JTextField tf ;
+	public JTextFieldSample() {
+		tf = new JTextField();
+		add(tf,BorderLayout.NORTH);
+		JButton jbok = new JButton("确定");
+		jbok.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String str = tf.getText();
+				if(str.equals(null))
+					println("OK");
+				
+			}
+		});
+		add(jbok,BorderLayout.CENTER);
+	}
+	public static void main(String[]args){
+		JTextFieldSample spl = new JTextFieldSample();
+		spl.setSize(300, 200);
+	}
+}
